bc73b93eb64996e4a78eab117d085640f0e4a104
gpasm <gpasami@sfsu.edu>
2025-02-25 21:03:33 -0800

    added comments

--- sculptureScript.js
+++ sculptureScript.js
@@ -1,23 +1,23 @@
-const sculptureList = require('./data.js'); // import data.js
+const sculptureList = require('./data.js');     // import data.js
 
-let counter = 0;
+let counter = 0;                                // counter to iterate loop
 
-const sculptureListLengths = [];
+const sculptureListLengths = [];           
 
-for(let i = 0; i < sculptureList.length; i++){
+for(let i = 0; i < sculptureList.length; i++){  // iterates through sculptureList elements
     const element = sculptureList[i];
-    const temp = {};
-    for (let key in element) {
-        if (element.hasOwnProperty(key)){
+    const temp = {};                            // placeholder for updating key lengths into sculptureListLengths element
+    for (let key in element) {                  // iterates through the keys in a sculptureList element
+        if (element.hasOwnProperty(key)){       // if the element owns its key, the key length is copied into the temp 
             temp[key] = element[key].length;
         }
     }
     
-    sculptureListLengths.push(temp);
-    counter += 1;
+    sculptureListLengths.push(temp);            // temp is added as an element to sculptureListLengths
+    counter += 1;                               // increase counter by 1
 }
 
-console.log(sculptureListLengths);
+console.log(sculptureListLengths);              // prints out the sculptureListLengths
 
 
 // this following snippet is just to show how to iterate an object's keys
